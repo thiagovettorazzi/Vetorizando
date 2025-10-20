52fb3fa8ca64464e3af6f4c2f92ccf8b18281d15
Thiago Vettorazzi <THIAGOVETTORAZZI@edu.unisinos.br>
2025-10-20 20:21:32 -0300

    topo do site pela metade

---
+++ gestao-de-marketing.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <?php
+        $titulo = "Gestão de marketing";
+        $meta_title = $titulo . " - Vetorizando";
+        
+        require_once("includes/head.php");
+        ?>
+    </head>
+
+    <body>
+        <main class="main">
+            <?php require_once("includes/topo.php"); ?>
+            
+            <section class="conteudo"></section>
+            
+            <?php require_once("includes/rodape.php"); ?>
+        </main>
+    </body>
+</html>
---
+++ identidade-visual.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <?php
+        $titulo = "Identidade visual";
+        $meta_title = $titulo . " - Vetorizando";
+        
+        require_once("includes/head.php");
+        ?>
+    </head>
+
+    <body>
+        <main class="main">
+            <?php require_once("includes/topo.php"); ?>
+            
+            <section class="conteudo"></section>
+            
+            <?php require_once("includes/rodape.php"); ?>
+        </main>
+    </body>
+</html>
