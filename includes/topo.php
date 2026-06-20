2ce4c2da440010900c05ebca0e1e5e8311ca6716
Thiago Vettorazzi <THIAGOVETTORAZZI@edu.unisinos.br>
2026-06-20 14:43:39 -0300

    organizando

--- includes/topo.php
+++ includes/topo.php
@@ -6,9 +6,9 @@
         <nav class="nav" id="nav">
             <a href="index.php" class="nav-link active">Início</a>
             <a href="sobre.php" class="nav-link">Sobre</a>
-            <a href="servicos.php" class="nav-link">Serviços</a>
+            <!-- <a href="servicos.php" class="nav-link">Serviços</a> -->
             <a href="calendario.php" class="nav-link">Calendário Pet</a>
-            <a href="contato.php" class="nav-link nav-cta">Fale Conosco</a>
+            <!-- <a href="contato.php" class="nav-link nav-cta">Fale Conosco</a> -->
         </nav>
         <button class="hamburger" id="hamburger" aria-label="Menu">
             <span></span><span></span><span></span>
